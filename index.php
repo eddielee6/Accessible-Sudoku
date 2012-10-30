39bdc97615e109cb7f7b4a9c1b05e4b3e9020ab5
Arron Jeffery <imarronjeffery@me.com>
2012-10-30 20:08:53 +0000

    Ready for development

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<h1>This is a test, again</h1>
+<!doctype html>
+<html lang="en">
+
+<head>
+	
+	<!-- TODO: Place head contents here -->
+	<meta charset="utf-8" />
+	<title>AAD Project - Empty</title>
+	
+	
+<body id="nothing" class="empty">
+	
+	<!-- TODO: Place body contents here -->
+	<p style="font-size: 24px; color:#3a3; text-align:center;"><code>// TODO: Put some implimentation here</code></p>
