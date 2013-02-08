229dc2f25cb1ce8657832300d83e6edee76cfa20
Eddie Lee <dudelee@gmail.com>
2013-02-08 11:53:12 +0000

    Started knockout binding. Added load game/new game stuff

--- sudoku-new/js/SudokuGameController.js
+++ sudoku-new/js/SudokuGameController.js
@@ -4,11 +4,18 @@ SudokuGameController = function () {
 	var viewModel = new SudokuViewModel();
 
 	this.StartNewGame = function() {
-
+		localStorage.SetValueForKey("gameSave", "TEMP");
+		this.BindToView();
+		console.log("new game");
 	};
 
 	this.LoadSavedGame = function(savedGame) {
+		this.BindToView();
+		console.log("load game");
+	};
 
+	this.BindToView = function() {
+		ko.applyBindings(null);
 	};
 
 	var init = new function() {
