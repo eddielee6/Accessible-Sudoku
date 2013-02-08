0297b68f2e887f4457e6f06e33c78014629d322f
Eddie Lee <dudelee@gmail.com>
2013-02-08 13:37:37 +0000

    Fix

--- sudoku-new/js/model/CellViewModel.js
+++ sudoku-new/js/model/CellViewModel.js
@@ -1,4 +1,4 @@
-var SudokuViewModel = function() {
+var CellViewModel = function() {
 	this.SolutionValue = ko.observable();
 	this.OrigionalValue = ko.observable();
 	this.CurrentValue = ko.observable();
