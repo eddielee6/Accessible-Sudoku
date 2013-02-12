322decd960632677b8c9dee12d1b759e2d3d0add
Eddie Lee <dudelee@gmail.com>
2013-02-12 22:39:40 +0000

    Optimisations for android

--- sudoku-new/js/main.js
+++ sudoku-new/js/main.js
@@ -1,7 +1,8 @@
 $(function () {
     
     //Mobile device detection
-    if(navigator.userAgent.toLowerCase().match(/mobile/i)) {
+    if(navigator.userAgent.toLowerCase().match(/mobile/i) ||
+        navigator.userAgent.toLowerCase().match(/android/i)) {
         $("html").addClass("mobile");
     } else {
         $("html").addClass("desktop");
