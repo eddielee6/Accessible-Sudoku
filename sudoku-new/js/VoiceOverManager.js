21450954ae5d7377b02043aef55686e58035cce2
Eddie Lee <dudelee@gmail.com>
2013-02-15 12:44:02 +0000

    Tidy

--- sudoku-new/js/VoiceOverManager.js
+++ sudoku-new/js/VoiceOverManager.js
@@ -1,23 +1,7 @@
 VoiceOverManager = function(outputTo) {
-	var sender = this;
-	var delay = 500; //ms
-	var messageBuffer = "";
 	var outputElementId = outputTo;
 
 	this.OutputMessage = function(message) {
-
 		$("#"+outputElementId).text(message);
-
-		
-		// if(messageBuffer == "") {
-		// 	messageBuffer = message;
-		// 	setTimeout(function() {
-		// 		console.log("VoiceOver: '" + messageBuffer + "'");
-		// 		$("#"+outputElementId).text(messageBuffer);
-		// 		messageBuffer = "";
-		// 	}, delay);
-		// } else {
-		// 	messageBuffer = messageBuffer + " " + message;
-		// }
 	};
 };
